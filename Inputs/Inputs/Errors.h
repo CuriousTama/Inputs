a02bce0a504db27194875e2eedeea34e0db32c7e
Tamaho <Tamaho@vivaldi.net>
2021-04-15 00:55:05 +0200

    rework error's colors handling

--- Inputs/Inputs/Errors.h
+++ Inputs/Inputs/Errors.h
@@ -1,30 +1,46 @@
-#pragma once
+#ifndef ERROR_H
+#define ERROR_H
+
 #include <iostream>
 
+#define FOREGROUND_DEFAULT		FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE		// default color of console text
+#define FOREGROUND_ORANGE		FOREGROUND_RED | FOREGROUND_GREEN						// red + green = orange
+#define FOREGROUND_LIGHT_RED	FOREGROUND_RED | FOREGROUND_INTENSITY					// red + Intensity = light red
+
 template <class _Elem>
-constexpr void DebugWarning(_Elem str)
-{
+constexpr void DebugWarning(_Elem str) {
 #if !NDEBUG
-	std::cerr << "\033[33m" << "DEBUG WARNING >> " << str << "\033[0m" << std::endl;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, FOREGROUND_ORANGE);
+	std::cerr << "DEBUG WARNING >> " << str << std::endl;
+	SetConsoleTextAttribute(hConsole, FOREGROUND_DEFAULT);
 #endif
 }
 
 template <class _Elem>
-constexpr void Warning(_Elem str)
-{
-	std::cerr << "\033[33m" << "WARNING >> " << str << "\033[0m" << std::endl;
+constexpr void Warning(_Elem str) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, FOREGROUND_ORANGE);
+	std::cerr << "WARNING >> " << str << std::endl;
+	SetConsoleTextAttribute(hConsole, FOREGROUND_DEFAULT);
 }
 
 template <class _Elem>
-constexpr void DebugError(_Elem str)
-{
+constexpr void DebugError(_Elem str) {
 #if !NDEBUG
-	std::cerr << "\033[91m" << "DEBUG ERROR >> " << str << "\033[0m" << std::endl;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, FOREGROUND_LIGHT_RED);
+	std::cerr << "DEBUG ERROR >> " << str << std::endl;
+	SetConsoleTextAttribute(hConsole, FOREGROUND_DEFAULT);
 #endif
 }
 
 template <class _Elem>
-constexpr void Error(_Elem str)
-{
-	std::cerr << "\033[91m" << "ERROR >> " << str << "\033[0m" << std::endl;
+constexpr void Error(_Elem str) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, FOREGROUND_LIGHT_RED);
+	std::cerr << "ERROR >> " << str << std::endl;
+	SetConsoleTextAttribute(hConsole, FOREGROUND_DEFAULT);
 }
+
+#endif // !ERROR_H
